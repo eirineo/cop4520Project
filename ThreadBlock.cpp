c022dba84f074d73996c5c6f98e76bd5beb1e547
eirineo <60438888+eirineo@users.noreply.github.com>
2022-04-27 21:47:16 -0400

    Allow threads to loop instead of being restarted

--- ThreadBlock.cpp
+++ ThreadBlock.cpp
@@ -18,32 +18,31 @@ string ThreadBlock::GetHash()
 
 void ThreadBlock::_CalculateHash()
 {
-    if(!lookingForHash)
+    while(lookingForHash)
     {
-        return;
-    }
-    string result = "";
-    stringstream ss;
-    int diff = 6;
-    char cstr[diff + 1];
-    for(uint32_t i = 0; i < diff; ++i)
-    {
-        cstr[i] = '0';
-    }
-    cstr[diff] = '\0';
-    string str(cstr);
-    
-    _nNonce++;
-    ss << _nIndex << _tTime << _sData << _nNonce << sPrevHash;
+        string result = "";
+        stringstream ss;
+        int diff = 6;
+        char cstr[diff + 1];
+        for(uint32_t i = 0; i < diff; ++i)
+        {
+            cstr[i] = '0';
+        }
+        cstr[diff] = '\0';
+        string str(cstr);
+        
+        _nNonce++;
+        ss << _nIndex << _tTime << _sData << _nNonce << sPrevHash;
 
-    result = sha256(ss.str());
+        result = sha256(ss.str());
 
-    if(str == result.substr(0, diff))
-    {
-        _sHash = result;
-        lookingForHash = false;
-    }
-}
+        if(str == result.substr(0, diff))
+        {
+            _sHash = result;
+            lookingForHash = false;
+        }//end if
+    }//end hwile loop
+}//end calchash
 
 void ThreadBlock::MultiThreadMine(uint32_t nDifficulty)
 {
@@ -70,25 +69,26 @@ void ThreadBlock::MultiThreadMine(uint32_t nDifficulty)
         blockThreads[i] = thread(&ThreadBlock::_CalculateHash,this);
     }
     //loops until hash is found
-    do
-    {   
-        //finds next available thread
-        for(nextIndex = 0;!blockThreads[nextIndex].joinable(); nextIndex++)
-        {
-            nextIndex++;
-            nextIndex %= numThreads;
-        }//end for loop
+    // do
+    // {   
+    //     //finds next available thread
+    //     for(nextIndex = 0;!blockThreads[nextIndex].joinable(); nextIndex++)
+    //     {
+    //         nextIndex++;
+    //         nextIndex %= numThreads;
+    //     }//end for loop
 
-        //retrieved promised value
-        // _sHash = fufilledGurantees[nextIndex].get();
-        //resets thread
-        blockThreads[nextIndex].join();
+    //     //retrieved promised value
+    //     // _sHash = fufilledGurantees[nextIndex].get();
+    //     //resets thread
+    //     blockThreads[nextIndex].join();
 
-        // promise<string> gurantee;
-        // fufilledGurantees[nextIndex] = gurantee.get_future();
-        blockThreads[nextIndex] = thread(&ThreadBlock::_CalculateHash, this);
+    //     // promise<string> gurantee;
+    //     // fufilledGurantees[nextIndex] = gurantee.get_future();
+    //     blockThreads[nextIndex] = thread(&ThreadBlock::_CalculateHash, this);
 
-    } while (lookingForHash);
+    // } while (lookingForHash);
+    while(lookingForHash){}
     for(int i = 0; i < numThreads; i++)
     {
 
